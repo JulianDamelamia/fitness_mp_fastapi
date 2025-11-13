244da9806fca4c7f4ab3e93b380ac7283e609b2b
Damian Piuselli <damianpiuselli@gmail.com>
2025-11-13 17:26:47 -0300

    switch entre base de datos local y en la nube. env variable para url de la base de datos

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Expone el puerto en el que correrá uvicorn (FastAPI)
-EXPOSE 8000
+EXPOSE 10000
 
 # Comando para correr la aplicación
 # OBSERVACION: --host 0.0.0.0 es OBLIGATORIO para que sea accesible desde fuera del contenedor
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "10000"]
--- app/db/session.py
+++ app/db/session.py
@@ -3,17 +3,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# cargamos las variables de entorno desde el archivo .env (si corremos localmente)
+load_dotenv()
+
 
 # 1. Definir la URL de la base de datos.
 # Esto le indica a SQLAlchemy que cree un archivo llamado 'test.db'
 # en la carpeta de tu proyecto.
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+
+    # Lógica para Render (PostgreSQL)
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
+
+    # Lógica para Local (MySQL)
+    # (Ya debería estar correcta en tu .env, pero esto es por si acaso)
+    elif DATABASE_URL.startswith("mysql://"):
+        DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
+else:
+    raise ValueError(
+        "DATABASE_URL no está definida. Asegúrate de tener un .env local o de que esté configurada en producción."
+    )
+
 
 # 2. Crear el engine de SQLAlchemy
 # El argumento 'check_same_thread' es necesario solo para SQLite.
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # 3. Crear la clase SessionLocal
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
